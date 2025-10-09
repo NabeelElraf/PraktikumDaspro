64ec1be01b9d1e6fd23e3de54284dfbc68e170f8
NabeelElraf <NabeelHE123@gmail.com>
2025-10-09 12:30:15 +0700

    Attempt 3

---
+++ JOBSHEET5/nestedUjianSkripsi18.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class nestedUjianSkripsi18 {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        String message = "";
+        
+        System.out.println("Has the student been cleared of compensation (Y/T or Yes/No)");
+        String compensationStatus = sc.nextLine().trim();
+
+        boolean free = compensationStatus.equalsIgnoreCase("Y") || compensationStatus.equalsIgnoreCase("YES");
+
+        System.out.print("Enter the number of guidance logs with Supervisor 1: ");
+        int supervisor1 = sc.nextInt();
+        System.out.print("Enter the number of guidance logs with Supervisor 2: ");
+        int supervisor2 = sc.nextInt();
+        
+
+    if (free) {
+        if (supervisor1 >= 8 && supervisor2 >= 4) 
+    {message = "All requirements are met. The student may register for the thesis exam.";}
+
+        else if (supervisor1 < 8 && supervisor2<4) 
+    {message = "Failed: Guidance logs with Supervisor 1 are fewer than 8 AND Supervisor 2 fewer than 4.";}
+    
+        else if (supervisor1 < 8 && supervisor2 >= 4) 
+    {message = "Failed: Guidance logs with Supervisor 1 are fewer than 8.";}
+        else if (supervisor1 >= 8 && supervisor2 < 4) 
+    {message = "Failed: Guidance logs with Supervisor 2 are fewer than 4.";}
+    }
+    else { message = "Failed: The student has not been cleared of compentastion.";}
+
+    System.out.println(message);
+    }
+}
