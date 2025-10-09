28256c96ed59595f5e6043e92654b76bd8fd1130
NabeelElraf <NabeelHE123@gmail.com>
2025-10-09 12:44:12 +0700

    Task 1

--- JOBSHEET5/ifCetakKRS18.java
+++ JOBSHEET5/ifCetakKRS18.java
@@ -8,7 +8,7 @@ public class ifCetakKRS18 {
         System.out.println("---print KRS SIAKAR---");
         System.out.println("Have a tuition fees been paid in full? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
-
+        
         if (uktLunas) {
             System.out.println("UKT payment has been verified");
             System.out.println("KRS now can be printed");
