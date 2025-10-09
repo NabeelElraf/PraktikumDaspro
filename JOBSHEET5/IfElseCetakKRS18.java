96ad411f346f07441287fbbb1454adebb3db082b
NabeelElraf <NabeelHE123@gmail.com>
2025-10-09 13:46:42 +0700

    Trial Modification 2

--- JOBSHEET5/IfElseCetakKRS18.java
+++ JOBSHEET5/IfElseCetakKRS18.java
@@ -1,8 +1,7 @@
-import java.util.Scanner;
-public class switchCetakKRS18 {
+import  java.util.Scanner; 
 
-
-    public static void main(String[] args) {
+public class IfElseCetakKRS18 {
+public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
 
@@ -43,6 +42,8 @@ public class switchCetakKRS18 {
         }
         
     }
+
+
 
 
 
